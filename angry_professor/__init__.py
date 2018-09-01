46d9226e6b9fe14c5352cfe7c7a1b84acb82a971
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-09-01 19:56:22 -0300

    angry professor solution

--- angry_professor/__init__.py
+++ angry_professor/__init__.py
@@ -6,22 +6,31 @@ import random
 import re
 import sys
 
+from os import path
+
+
 # Complete the angryProfessor function below.
 def angryProfessor(k, a):
     count = int(0)
+    print('a length: %i' % len(a))
     for i in a:
-        if i >= 0:
+        if i <= 0:
             count = int(count) + int(1)
-    
-    if count <= k:
-        return "YES"
+    print('count: %i' % count)
+    print('k: %i' % k)
+    if count < k:
+        return 'YES'
     else:
-        return "NO"
+        return 'NO'
+
 
 if __name__ == '__main__':
-    fptr = open('output02.txt', 'w')
-    fptrInput = open('input03.txt', 'w')
-    t = fptrInput.readline() # int(input())
+    script_dir = os.path.dirname(__file__)
+
+    fptrInput = open(os.path.join(script_dir, './input01.txt'), 'r')
+    fptr = open(os.path.join(script_dir, './output03.txt'), 'w')
+    
+    t = int(fptrInput.readline()) # int(input())
 
     for t_itr in range(t):
         nk = fptrInput.readline().split()
@@ -30,10 +39,10 @@ if __name__ == '__main__':
 
         k = int(nk[1])
 
-        a = list(map(int, fptrInput().readline().rstrip().split()))
+        a = list(map(int, fptrInput.readline().rstrip().split()))
 
         result = angryProfessor(k, a)
-
+        print('Result: %s' % result)
         fptr.write(result + '\n')
 
     fptr.close()

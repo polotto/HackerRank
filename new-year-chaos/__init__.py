b2ca0e6858954740034ef8aaebb780f92f9a19d2
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-09-12 14:01:04 -0300

    new year chaos sol 2

--- new-year-chaos/__init__.py
+++ new-year-chaos/__init__.py
@@ -12,22 +12,47 @@ def minimumBribes(q):
     for i in range(1, len(q) + 1):
         for j in range(1, len(q) + 1):
             p = q[j-1]
+            print(i, p, j)
             if p == i and j < i:
                 d.append(abs(j - i))
     
+    print(d)
+
     for k in d:
         if k > 2:
             print('Too chaotic')
+            return
     ans = sum(d)
     print(ans)
                 
 
 if __name__ == '__main__':
-    t = int(input())
+    inp = '''2
+5
+2 1 5 3 4
+5
+2 5 1 3 4
+
+8
+
+5 - 2
+7 - 2
+8 - 2
+6 - 1
+1 2 3 4 5 6 7 8
+
+1 2 5 3 4 6 7 8
+1 2 5 3 7 4 6 8
+1 2 5 3 7 8 4 6
+
+1 2 5 3 7 8 6 4
+    '''
+
+    t = int(1)
 
     for t_itr in range(t):
-        n = int(input())
+        n = int(8)
 
-        q = list(map(int, input().rstrip().split()))
+        q = list(map(int, '1 2 5 3 7 8 6 4'.rstrip().split()))
 
         minimumBribes(q)

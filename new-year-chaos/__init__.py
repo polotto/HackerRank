5d6e47a629c2badca846404c281fdeccca980da1
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-09-18 21:15:32 -0300

    solution 2 new year chaos

--- new-year-chaos/__init__.py
+++ new-year-chaos/__init__.py
@@ -8,56 +8,55 @@ import sys
 
 # Complete the minimumBribes function below.
 def minimumBribes(q):
+    r = list(range(1, len(q) + 1))
     d = []
-    for i in range(1, len(q) + 1):
-        for j in range(1, len(q) + 1):
-            p = q[j-1]
-            if p == i and j < i:
-                d.append(abs(j - i))
-            elif p == i and len(q) > j and q[j] < p :
-                print(i, p, j)
-                d.append(abs(j - i))
-    
-    print(d)
-
-    for k in d:
-        if k > 2:
-            print('Too chaotic')
-            return
+    for i in range(len(q)):
+        if r[i] != q[i]:
+            print(r)
+            ir = r.index(q[i], q[i] - 1)
+            print(ir, q[i] - 1)
+            a = r.pop(ir)
+            r.insert(i, a)
+            d.append(abs(ir - i))
+            print(q)
+            print(r)
+            print()
+    if max(d) > 2:
+        print('Too chaotic')
+        return
     ans = sum(d)
     print(ans)
+    
+##    d = []
+##    for i in range(1, len(q) + 1):
+##        for j in range(1, len(q) + 1):
+##            p = q[j-1]
+##            if p == i and j < i:
+##                d.append(abs(j - i))
+##            elif p == i and len(q) > j and q[j] < p :
+##                print(i, p, j)
+##                d.append(abs(j - i))
+##    
+##    print(d)
+##
+##    for k in d:
+##        if k > 2:
+##            print('Too chaotic')
+##            return
+##    ans = sum(d)
+##    print(ans)
                 
 
 if __name__ == '__main__':
-    inp = '''2
-5
-2 1 5 3 4
-5
-2 5 1 3 4
+    dir_scr = os.path.dirname(__file__)
+    input_tests = open(os.path.join(dir_scr, './input09.txt'), 'r')
+    
+    
+    t = int(input_tests.readline())
 
-8
+    for _ in range(t):
+        n = int(input_tests.readline())
 
-5 - 2
-7 - 2
-8 - 2
-6 - 1
-
-1 2 5 3 7 8 6 4
-
-1 2 3 4 5 6 7 8
-
-1 2 5 3 4 6 7 8
-1 2 5 3 7 4 6 8
-1 2 5 3 7 8 4 6
-
-1 2 5 3 7 8 6 4
-    '''
-
-    t = int(1)
-
-    for t_itr in range(t):
-        n = int(8)
-
-        q = list(map(int, '1 2 5 3 7 8 6 4'.rstrip().split()))
+        q = list(map(int, input_tests.readline().rstrip().split()))
 
         minimumBribes(q)

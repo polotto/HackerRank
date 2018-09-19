f8752e19eca5c7e2be12b4fdf013b34d56349c2a
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-09-19 14:55:14 -0300

    solution using merge sort

--- new-year-chaos/__init__.py
+++ new-year-chaos/__init__.py
@@ -1,30 +1,81 @@
 #!/bin/python3
 
+from timeit import default_timer as timer
+
 import math
 import os
 import random
 import re
 import sys
 
+def mergeSort(alist, ans):
+    # print("Splitting ",alist)
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf, ans)
+        mergeSort(righthalf, ans)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] > righthalf[j]:
+                ans[0] += 1
+            if lefthalf[i] < righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    # print("Merging ",alist)
+
 # Complete the minimumBribes function below.
 def minimumBribes(q):
+    ans = [0]
+
+    # check if the queue is too chaotic
+    for i, v in enumerate(q):
+        if (v - 1) - i > 2:
+            print('Too chaotic')
+            return
+
+    mergeSort(q, ans)
+    print(ans)
+
+def minimumBribes2(q):
     r = list(range(1, len(q) + 1))
-    d = 0
-    for i in range(len(q)):
-        if r[i] != q[i]:
+    ans = 0
+    i = 0
+    for i in range(len(q)-1):
+        qi = q[i]
+        if r[i] != qi:
             #print(r)
-            ir = r.index(q[i], q[i] - 1, q[i] + 10)
+            ir = r.index(qi, qi - 1, qi + 6)
             #print(ir, q[i] - 1)
-            a = r.pop(ir)
-            r.insert(i, a)
-            
-            if abs(ir - i) > 2:
+            r.insert(i, r.pop(ir))
+            bribes = abs(ir - i)
+            if bribes > 2:
                 print('Too chaotic')
                 return
-            d = d + abs(ir - i)
-    print(d)
+            ans = ans + bribes
+    print(ans)
 
 if __name__ == '__main__':
+    start = timer()
     dir_scr = os.path.dirname(__file__)
     input_tests = open(os.path.join(dir_scr, './input08.txt'), 'r')
     
@@ -36,3 +87,6 @@ if __name__ == '__main__':
         q = list(map(int, input_tests.readline().rstrip().split()))
 
         minimumBribes(q)
+    
+    end = timer()
+    print(end-start)

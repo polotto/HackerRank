4240da249b33368fa82bcc645314c877b6dbdeea
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-09-24 18:23:58 -0300

    final solution array manipulation

--- array-manipulation/__init__.py
+++ array-manipulation/__init__.py
@@ -7,15 +7,58 @@ import re
 import sys
 from timeit import default_timer as timer
 
+'''
+instead of storing the actual values in the array, 
+you store the difference between the current element 
+and the previous element. So you add sum to a[p] showing 
+that a[p] is greater than its previous element by sum. 
+You subtract sum from a[q+1] to show that a[q+1] is less 
+than a[q] by sum (since a[q] was the last element that was 
+added to sum). By the end of all this, you have an array 
+that shows the difference between every successive element. 
+By adding all the positive differences, you get the value 
+of the maximum element.
+'''
+def arrayManipulation2(n, queries):
+        # Big O (N)
+        res = [0]*(n+1) # we only really need one terminal row, since we're applying each pass to all rows below
+
+        # loop through all the queries and apply the increments/decrements for each
+        # Big O (M) (size of queires)
+        for row in range(len(queries)):
+                a = queries[row][0]
+                b = queries[row][1]
+                k = queries[row][2]
+
+                res[a-1] += k # increment the starting position
+                # this is where a loop would increment everything else between a & b by k
+                # but instead of taking b-a steps, we take a constant 2 steps, saving huge on time
+                res[b] -= k # decrement the position AFTER the ending position
+
+        # now loop through res one time - Big O (N) (size of res)
+        sm = 0 # running sum
+        mx = 0 # maximum value found so far
+        for i in range(len(res)):
+                sm += res[i]
+                if sm > mx:
+                        mx = sm
+
+        # total run time is Big O (2*N + M) >> Big O(N)
+        return mx
+
 # Complete the arrayManipulation function below.
 def arrayManipulation(n, queries):
-    maximum = 0
-    q_previous = [0,n]
-    for q in queries:
-        #if q[0] >= q_previous[0] and q[1] <= q_previous[1]:
-        maximum += q[2]
-        #q_previous = q
+    arr = [0]*(n+1)
     
+    for q in queries:
+        arr[q[0]-1] += q[2]
+        arr[q[1]] -= q[2]
+
+    sum_arr = 0
+    maximum = 0
+    for i in arr:
+        sum_arr += i
+        maximum = max(sum_arr, maximum)
     return maximum
 
 def arrayManipulationBruteForce(n, queries):

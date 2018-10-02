80a206f05bbc352e2ff3b5f8791353d348c4db68
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-10-02 18:21:37 -0300

    frequency queries solution for the most of test cases

--- frequency-queries/__init__.py
+++ frequency-queries/__init__.py
@@ -6,30 +6,37 @@ import random
 import re
 import sys
 from collections import defaultdict
+from timeit import default_timer as timer
 
 # Complete the freqQuery function below.
 def freqQuery(queries):
-    count_data = defaultdict(int)
-    data = []
+    # count_data = defaultdict(int)
+    data = defaultdict(int)
+    output = []
     for q in queries:
         if q[0] == 1:
-            data.append(q[1])
-            count_data[q[1]] += 1
+            data[q[1]] += 1
         elif q[0] == 2 and (q[1] in data):
-            del data[data.index(q[1])]
-            count_data[q[1]] += 1
-        else:
-            count_data.
+            data[q[1]] -= 1
+        elif q[0] == 3:
+            if q[1] in data.values():
+                output.append(1)
+            else:
+                output.append(0)
+    return output
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    start = timer()
+    scr_dir = os.path.dirname(__file__)
+    fptr = open(os.path.join(scr_dir, './output/output.txt'), 'w')
+    fptr_input = open(os.path.join(scr_dir, './input/input09.txt'), 'r')
 
-    q = int(input().strip())
+    q = int(fptr_input.readline().strip())
 
     queries = []
 
     for _ in range(q):
-        queries.append(list(map(int, input().rstrip().split())))
+        queries.append(list(map(int, fptr_input.readline().rstrip().split())))
 
     ans = freqQuery(queries)
 
@@ -37,3 +44,6 @@ if __name__ == '__main__':
     fptr.write('\n')
 
     fptr.close()
+
+    # print(ans)
+    print(timer() - start)

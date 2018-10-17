56b1eff9289852ecacd8d34623f264e535084039
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-10-17 09:46:38 -0300

    final solution sherlock and valid string

--- string-manipulation/sherlock-and-valid-string/__init__.py
+++ string-manipulation/sherlock-and-valid-string/__init__.py
@@ -9,24 +9,37 @@ from collections import Counter
 
 # Complete the isValid function below.
 def isValid(s):
+    # frequencies of all letters
     freq = Counter(s)
+    # frequencies values sorted
     values = sorted(freq.values())
+    # max frequencie value
     v_max = max(values)
+    # min frequencie value
     v_min = min(values)
+    # max frequencie value repetitions
     max_count = values.count(v_max)
+    # min frequencie value repetitions
     min_count = values.count(v_min)
+    # frequencie of frequencie values, made some validations more easy 
+    freq_values = Counter(values)
 
-    print(values)
-    print(v_max, v_min)
-    print(max_count, min_count)
-
-    if min_count == 1:
+    # more then 2 frequencies repeat, this case string can be valid, case: aaabbc
+    if len(freq_values) > 2:
+        return 'NO'
+    # only 1 frequencie repeat, so, is a valid string, case: aabbcc
+    elif len(freq_values) == 1:
         return 'YES'
+    # min frequencie count is 1, so, just remove one value to turn the string valid, case: aab
+    elif min_count == 1:
+        return 'YES'
+    # max frequencie minus your repeatitions equal min value, case: bbaaa, just remove
+    # one of the same letter repeatitions to turn the strig valid
     elif v_max - max_count == v_min:
         return 'YES'
+    # other cases are not valids
     else:
         return 'NO'
-
 
 if __name__ == '__main__':
     tests = ['00', '01', '03', '04', '18', '06']

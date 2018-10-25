1d3dcb00b76d32e4e7c48bd870a29ced3cf7cc58
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-10-24 21:07:56 -0300

    initial implementation reverse shuffle

---
+++ greedy-algorithms/reverse-shuffle-merge/__init__.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def reverse(s):
+    return s[::-1]
+
+def diff(s, r):
+
+# Complete the reverseShuffleMerge function below.
+def reverseShuffleMerge(s):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = reverseShuffleMerge(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()

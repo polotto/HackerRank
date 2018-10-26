2a189d77802051a7b8b5397d4f7b273de159998d
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-10-25 22:51:15 -0300

    secound solution reverse shuffle merge

--- greedy-algorithms/reverse-shuffle-merge/__init__.py
+++ greedy-algorithms/reverse-shuffle-merge/__init__.py
@@ -5,22 +5,46 @@ import os
 import random
 import re
 import sys
+from itertools import permutations
 
 def reverse(s):
     return s[::-1]
 
 def diff(s, r):
+    ans = s
+    for j in r:
+        ans = ans.replace(j,'', 1)
+    return ans
+
+def rotate(s, n):
+    return s[-n:] + s[:-n]
 
 # Complete the reverseShuffleMerge function below.
 def reverseShuffleMerge(s):
+    N = len(s)
+    s1 = s[:N//2]
+    # print(s1, s2)
+    r_s1 = reverse(s1)
+    d_s = diff(s, r_s1)
+    print(r_s1, d_s)
+    # perms = [''.join(p) for p in permutations(s1)]
+    # perms = sorted(perms)
+    # print(perms)
+    ans = r_s1
+    while ans >= s1:
+        print(ans)
+        ans = rotate(ans, 1)
+    return ans
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    test_case = '15'
+    sc_dir = os.path.dirname(__file__)
+    fp_in = open(os.path.join(sc_dir, './input/input%s.txt' % test_case), 'r')
+    fp_test = open(os.path.join(sc_dir, './output/output%s.txt' % test_case), 'r')
 
-    s = input()
+    s = fp_in.readline().rstrip()
 
     result = reverseShuffleMerge(s)
+    test_result = fp_test.readline().strip()
 
-    fptr.write(result + '\n')
-
-    fptr.close()
+    print(result, test_result, result == test_result)

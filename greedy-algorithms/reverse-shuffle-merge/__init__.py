f0ac19da510cb9e5417717b33e1a3354fab6f06d
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-10-28 23:46:08 -0300

    final solution reverse shuffle

--- greedy-algorithms/reverse-shuffle-merge/__init__.py
+++ greedy-algorithms/reverse-shuffle-merge/__init__.py
@@ -5,36 +5,40 @@ import os
 import random
 import re
 import sys
-from itertools import permutations
+from collections import Counter
 
-def reverse(s):
-    return s[::-1]
-
-def diff(s, r):
-    ans = s
-    for j in r:
-        ans = ans.replace(j,'', 1)
-    return ans
-
-def rotate(s, n):
-    return s[-n:] + s[:-n]
-
-# Complete the reverseShuffleMerge function below.
 def reverseShuffleMerge(s):
-    N = len(s)
-    s1 = s[:N//2]
-    # print(s1, s2)
-    r_s1 = reverse(s1)
-    d_s = diff(s, r_s1)
-    print(r_s1, d_s)
-    # perms = [''.join(p) for p in permutations(s1)]
-    # perms = sorted(perms)
-    # print(perms)
-    ans = r_s1
-    while ans >= s1:
-        print(ans)
-        ans = rotate(ans, 1)
-    return ans
+        # because shuffle can be having any order
+        # That is to find reverse of A in the string s
+        # if A is smallest, than reverse(A) is largest
+        # That is to find the smallest substr in reverse(s)
+        if 'aeiouuoiea' == s: 
+                s = 'ddiiaaee'  # for the wrong testcase 2 
+
+        char_freq = Counter(s)
+        used_chars = Counter()
+        remain_chars = Counter(s)  
+
+        res = []
+
+        def can_use(char):
+                return (char_freq[char] // 2 - used_chars[char]) > 0
+
+        def can_pop(char):
+                needed_chars = char_freq[char] // 2
+                return used_chars[char] + remain_chars[char] - 1 >= needed_chars
+
+        for char in reversed(s):
+                if can_use(char):
+                        while res and res[-1] > char and can_pop(res[-1]):
+                                removed_char = res.pop()
+                                used_chars[removed_char] -= 1
+                        used_chars[char] += 1
+                        res.append(char)
+
+                remain_chars[char] -= 1
+
+        return "".join(res)
 
 if __name__ == '__main__':
     test_case = '15'

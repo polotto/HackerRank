34e0249e6bf0518b6e5f2e3479e177161d7212f5
Ângelo Polotto <angelopolotto@yahoo.com.br>
2018-10-30 15:57:33 -0300

    final solution ice cream parlor

--- search/ctci-ice-cream-parlor/__init__.py
+++ search/ctci-ice-cream-parlor/__init__.py
@@ -5,6 +5,7 @@ import os
 import random
 import re
 import sys
+from timeit import default_timer as timer
 
 # Complete the whatFlavors function below.
 def whatFlavors2(cost, money):
@@ -24,32 +25,55 @@ def whatFlavors2(cost, money):
     if ice_a[0] > ice_b[0]:
         ice_a, ice_b = ice_b, ice_a
     print(ice_a[0] + 1, ice_b[0] + 1)
+    
+# http://interactivepython.org/runestone/static/pythonds/SortSearch/TheBinarySearch.html
+def binarySearch(elem, a):
+    left = 0
+    right = len(a) - 1
+    mid_i = None
+    mid = None
+    found = False
+    while left <= right and not found:
+        mid_i = (left + right)//2
+        mid = a[mid_i]
+        if mid[1] == elem:
+            found = True
+        elif elem > mid[1]:
+            left = mid_i + 1
+        elif elem < mid[1]:
+            right = mid_i - 1
+    
+    return mid
 
-def whatFlavors(cost, money):
-    cost_s = sorted(enumerate(filter(lambda x: x < money, cost)), key = lambda x: x[1])
-    N = len(cost_s)
+def whatFlavors3(cost, money):
+    cost_f = cost
+    cost_enum = list(enumerate(cost))
+    N = len(cost_enum)
     ice_a = None
     ice_b = None
     for i in range(N):
-        ice_a = cost_s[i]
-        found = False
-        for j in range(i+1, N):
-            ice_b = cost_s[j]
-            total = ice_a[1] + ice_b[1]
-            if money == total:
-                found = True
-                break
-            elif total > money:
-                break
-        
-        if found:
+        ice_a = cost_enum[i]
+        ice_b_price = money - ice_a[1]
+        if ice_b_price in cost_f[i+1:]:
+            ice_b = list(filter(lambda x: x[1] == ice_b_price, cost_enum[i+1:]))[0]
             break
     
     if ice_a[0] > ice_b[0]:
         ice_a, ice_b = ice_b, ice_a
     print(ice_a[0] + 1, ice_b[0] + 1)
 
+def whatFlavors(cost, money):
+    map = {}
+    for i, v in enumerate(cost):
+        ice_b = money - v
+        if ice_b in map:
+            print(map[ice_b], i + 1)
+            break
+        else:
+            map[v] = i + 1
+
 if __name__ == '__main__':
+    start = timer()
     test_case = '00'
     scr_dir = os.path.dirname(__file__)
     fp_in = open(os.path.join(scr_dir, './input/input%s.txt' % test_case), 'r')
@@ -63,3 +87,5 @@ if __name__ == '__main__':
         cost = list(map(int, fp_in.readline().rstrip().split()))
 
         whatFlavors(cost, money)
+
+    print(timer()-start)
